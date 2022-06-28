a13d2e10a4832a495dffde6743615f375f340602
Adlizm <adlizm.pq@gmail.com>
2022-06-28 17:14:46 -0300

    update admin\cli\client

--- src/admin/cli/client.rs
+++ src/admin/cli/client.rs
@@ -5,15 +5,15 @@ use sd_project::utils::response::Response;
 use sd_project::utils::inputs::{USE_CID, USE_BODY};
 
 pub fn create(stream: &TcpStream) -> Response {
-	make_request(stream, RequestType::CreateTask, USE_CID|USE_BODY)
+	make_request(stream, RequestType::CreateClient, USE_CID|USE_BODY)
 }
 pub fn update(stream: &TcpStream)-> Response {
-	make_request(stream, RequestType::UpdateTask, USE_CID|USE_BODY)
+	make_request(stream, RequestType::UpdateClient, USE_CID|USE_BODY)
 }
 pub fn get(stream: &TcpStream) -> Response{
-	make_request(stream, RequestType::ListTasks, USE_CID)
+	make_request(stream, RequestType::GetClient, USE_CID)
 }
 pub fn delete(stream: &TcpStream) -> Response{
-	make_request(stream, RequestType::DeleteTask, USE_CID)
+	make_request(stream, RequestType::DeleteClient, USE_CID)
 }
 
